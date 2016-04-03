beb340acb39461f004b57e07b975bd359ec7033b
Matt Hampton <hammy025@gmail.com>
2016-04-03 05:52:44 -0700

    Added some test code for simulation.

--- rtl/_common/fifo/verif/env/top.sv
+++ rtl/_common/fifo/verif/env/top.sv
@@ -13,7 +13,21 @@
 #
 ###############################################################################
 */
+`timescale 1ns/10ps
 module top();
+
+  logic clk_r, clk_w;
+  logic rst_r, rst_w;
+
+  initial begin
+    $dumpfile("sim.vcd");
+    $dumpvars;
+    clk_w = '0;
+    repeat(10) begin
+      $display("tick...");
+      #1ns clk_w = ~clk_w;
+    end
+  end
 
 endmodule
 
