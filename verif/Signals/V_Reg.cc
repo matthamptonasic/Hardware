58befbdafda9ffe6cca23c90d2471fb685427878
matthamptonasic <matthamptonasic@gmail.com>
2016-09-16 20:22:55 -0700

    Initial placeholders.

---
+++ verif/Signals/V_Reg.cc
@@ -0,0 +1,18 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   V_Reg.cc 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   09/16/16
+#   Description   :   
+#
+###############################################################################
+*/
+
+#include "V_Reg.h"
+
---
+++ verif/Signals/V_Signal.h
@@ -0,0 +1,27 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   V_Signal.h 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   09/16/16
+#   Description   :   
+#
+###############################################################################
+*/
+#ifndef V_SIGNAL_H
+#define V_SIGNAL_H
+
+#include "Common.h"
+
+class V_Signal 
+{
+
+};
+
+#endif /* V_SIGNAL_H */
+
