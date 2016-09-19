fa64b8675660c6a611c79b130d584df9a67bebdb
matthamptonasic <matthamptonasic@gmail.com>
2016-09-19 03:02:50 -0700

    Unified the naming. When calling the local Vpi class properties, use the scope operator per normal. When calling the global methods, specify the global scope. (e.g. ::vpi_get_value).

--- verif/Pli/vpi.cc
+++ verif/Pli/vpi.cc
@@ -18,9 +18,9 @@
 
 #include "vpi.h"
 
-vpiHandle Vpi::vpi_register_systf(const struct Vpi::vpi_systf_data *ss)
+vpiHandle Vpi::vpi_register_systf(const struct Vpi::t_vpi_systf_data *ss)
 {
   // Call the global function.
-  return ::vpi_register_systf((t_vpi_systf_data*)ss);
+  return ::vpi_register_systf((::t_vpi_systf_data*)ss);
 }
 
