2f97bd375e7939dc60f4cdf5bd6626cadea37c96
matthamptonasic <matthamptonasic@gmail.com>
2016-09-18 23:54:51 -0700

    WIP - Some basic functionality.

--- verif/Signals/V_Signal.cc
+++ verif/Signals/V_Signal.cc
@@ -14,6 +14,42 @@
 ###############################################################################
 */
 
+#include <iostream>
+
 #include "V_Signal.h"
+
+// Constructors
+V_Signal::V_Signal(string iFullName)
+{
+  init(iFullName, V_Signal::NB_STATES::FOUR_STATE);
+}
+
+// Inits
+bool V_Signal::init(string iFullName, V_Signal::NB_STATES iValue)
+{
+  m_name_full = iFullName;
+  m_nb_states = iValue;
+
+  return setHandle();
+}
+
+// Public Methods
+
+// Private Methods
+bool V_Signal::setHandle()
+{
+  vpiHandle temp = vpi_handle_by_name(m_name_full.c_str(), NULL);
+  if(temp == NULL) 
+  {
+    //cout << "Could not find signal '" << m_name_full << "'" << endl;
+    vpi_printf("Could not find signal '%s'.\n", m_name_full.c_str());
+  }
+  else
+  {
+    //cout << "Found signal '" << m_name_full << "'" << endl;
+    vpi_printf("Found signal '%s'.\n", m_name_full.c_str());
+  }
+  return true;
+}
 
 
--- verif/Signals/V_Signal.h
+++ verif/Signals/V_Signal.h
@@ -16,11 +16,45 @@
 #ifndef V_SIGNAL_H
 #define V_SIGNAL_H
 
+#include <string>
+
 #include "Common.h"
+#include "vpi_user.h"
+
+using namespace std;
 
 class V_Signal 
 {
+  // Enums
+  public:
+  enum class NB_STATES : Byte
+  {
+    TWO_STATE = 0,
+    FOUR_STATE = 1
+  };
 
+  // Members
+  private:
+    string m_name;
+    string m_name_full;
+    vpiHandle m_sigHandle;
+
+    NB_STATES m_nb_states;
+
+  // Constructors
+  public:
+    V_Signal(string iFullName);
+
+  // Inits
+  private:
+    bool init(string iFullName, V_Signal::NB_STATES iValue);
+
+  // Public Methods
+  public:
+
+  // Private Methods
+  private:
+    bool setHandle();
 };
 
 #endif /* V_SIGNAL_H */
