ed4959f3cca2d3a8a9ce8dab6c36623d2fcc352b
matthamptonasic <matthamptonasic@gmail.com>
2016-09-21 02:27:20 -0700

    Worked out the set handle function.

--- verif/Signals/V_Signal.cc
+++ verif/Signals/V_Signal.cc
@@ -17,11 +17,15 @@
 #include <iostream>
 
 #include "V_Signal.h"
+#include "vpi_entry.h"
+#include "pli.h"
 
 // Constructors
 V_Signal::V_Signal(string iFullName)
 {
   init(iFullName, V_Signal::NB_STATES::FOUR_STATE);
+  UInt32 curVal = (UInt32)Pli::GetScalar(m_sigHandle);
+  vpi_printf("Scalar value is 0x%x.\n", curVal);
 }
 
 // Inits
@@ -34,12 +38,20 @@ bool V_Signal::init(string iFullName, V_Signal::NB_STATES iValue)
 }
 
 // Public Methods
+UInt32 V_Signal::GetValue()
+{
+  UInt32 retVal = 0xffffffff;
+
+  return retVal;
+}
 
 // Private Methods
 bool V_Signal::setHandle()
 {
-  vpiHandle temp = vpi_handle_by_name(m_name_full.c_str(), NULL);
-  if(temp == NULL) 
+
+  vpi_printf("Looking for signal '%s'.\n", m_name_full.c_str());
+  m_sigHandle = Vpi::vpi_handle_by_name(m_name_full.c_str(), vpi_entry::TopModule_get());
+  if(m_sigHandle == NULL) 
   {
     //cout << "Could not find signal '" << m_name_full << "'" << endl;
     vpi_printf("Could not find signal '%s'.\n", m_name_full.c_str());
@@ -48,6 +60,8 @@ bool V_Signal::setHandle()
   {
     //cout << "Found signal '" << m_name_full << "'" << endl;
     vpi_printf("Found signal '%s'.\n", m_name_full.c_str());
+    vpi_printf("top hd is '0x%x'.\n", (UInt32)(UInt64)vpi_entry::TopModule_get());
+    vpi_printf("Handle is '0x%x'.\n", (UInt32)(UInt64)m_sigHandle);
   }
   return true;
 }
