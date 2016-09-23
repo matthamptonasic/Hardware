8e349df8926bf460b9f2f2b0617e66bc27dfe6d5
matthamptonasic <matthamptonasic@gmail.com>
2016-09-22 18:43:33 -0700

    Added some basic functionality. Added notes about how/why we're using composition instead of inheritance.

--- verif/BitVector/BitVector.cc
+++ verif/BitVector/BitVector.cc
@@ -16,10 +16,48 @@
 
 #include "BitVector.h"
 
-BitVector::BitVector(UInt32 iSize)
+BitVector::NB_STATES BitVector::g_nbStates = BitVector::NB_STATES::FOUR_STATE;
+
+BitVector::BitVector(string iName, UInt32 iSize)
 {
-  m_size = iSize;
-  m_lo = 0;
-  m_hi = iSize - 1;
+  init(iName, iSize, g_nbStates);
 }
 
+void BitVector::init(string iName, UInt32 iSize, NB_STATES iStates)
+{
+  m_name = iName;
+  m_size = iSize;
+  m_nbStates = iStates;
+  Int32 nbWds = (iSize - 1) / 32 + 1; 
+  m_aval = new vector<UInt32>(nbWds, 0);
+  if(m_nbStates == BitVector::NB_STATES::FOUR_STATE)
+  {
+    m_bval = new vector<UInt32>(nbWds, 0);
+  }
+}
+
+UInt32 BitVector::GetUInt32()
+{
+  return (*m_aval)[0];
+}
+UInt64 BitVector::GetUInt64()
+{
+  // TBD - Add size checking as well as masking if < 64b.
+  UInt64 retVal = 0;
+  UInt64 hi = (*m_aval)[1]; 
+  retVal = hi << 32;
+  retVal |=  (*m_aval)[0];
+  return retVal;
+}
+
+BitVector & BitVector::operator= (Int32 iRhs)
+{
+  if(m_size <= 0)
+  {
+    // TBD - log error.
+    return *this;
+  }
+  UInt32 temp = (UInt32)iRhs;
+  (*m_aval)[0] = temp;
+  return *this;
+}
--- verif/BitVector/BitVector.h
+++ verif/BitVector/BitVector.h
@@ -9,14 +9,43 @@
 #   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
 #   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
 #   Creation Date :   09/15/16
-#   Description   :   
+#   Description   :   A class to contain an n-bit vector as well as 
+#                     (optionally) mirroring/updating an equivalent wire/reg
+#                     in the Verilog hierarchy.
+#
+#                     Note on composition vs inheritance:
+#                       Normally we would create a BitVector class that would
+#                       not have any of the Verilog transport (PLI), then 
+#                       simply inherit from BitVector another class that would
+#                       have the Verilog mirror logic.
+#                       The problem I see is that, the child class would have
+#                       to override each of the BitVector methods in order to
+#                       add the Verilog transport functionality. That's not a
+#                       big deal. However, if someone adds a function or 
+#                       operator overload to BitVector and forgets to add the
+#                       same override in the child class, the child class will
+#                       still appear to work in the C-world, but fail to
+#                       reflect in the Verilog world. This could be very time
+#                       consuming and frustrating.
+#                       Instead, we're using composition so that a containing
+#                       class 'has a' BitVector. The same operator overload and 
+#                       access functions still must be implemented in the 
+#                       containing class, but the issue stated above will not 
+#                       happen. If the operator/function is not implemented
+#                       in the containing class, it will result in a 
+#                       compile error, not a runtime bug.
 #
 ###############################################################################
 */
 #ifndef BITVECTOR_H
 #define BITVECTOR_H
 
+#include <string>
+#include <vector>
+
 #include "Common.h"
+
+using namespace std;
 
 class BitVector {
   // Enums
@@ -27,15 +56,39 @@ class BitVector {
     FOUR_STATE = 1
   };
 
+  // Static Members
+  private:
+    static NB_STATES g_nbStates;
+
   // Private Members
   private:
     UInt32 m_size;
-    UInt32 m_lo;
-    UInt32 m_hi;
+    UInt32 m_mask;
+    string m_name;
+    vector<UInt32> * m_aval;
+    vector<UInt32> * m_bval;
+    NB_STATES m_nbStates;
 
   // Constructors
   public:
-    BitVector(UInt32 iSize);
+    BitVector(string iName, UInt32 iSize);
+
+  // Inits
+  private:
+    void init(string iName, UInt32 iSize, NB_STATES iStates);
+
+  // Public Methods
+  public:
+    UInt32 GetUInt32();
+    UInt64 GetUInt64();
+
+  // Private Methods
+  private:
+
+  // Operators
+  public:
+  BitVector & operator= (Int32 iRhs);
+
 
 };
 
