f03aed4568656c9ba3d4fc81719186f865fbcfa5
matthamptonasic <matthamptonasic@gmail.com>
2016-09-27 03:00:08 -0700

    Initial placeholder.

---
+++ verif/Logging/Logger.cc
@@ -0,0 +1,17 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Logger.cc 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   09/27/16
+#   Description   :   
+#
+###############################################################################
+*/
+
+#include "Logger.h"
