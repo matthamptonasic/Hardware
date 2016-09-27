a5e846815fb73f8b347e47d88657517b2a8f735a
matthamptonasic <matthamptonasic@gmail.com>
2016-09-27 06:34:15 -0700

    Scope class and basic members skeleton.

--- verif/Logging/Logger.cc
+++ verif/Logging/Logger.cc
@@ -15,3 +15,122 @@
 */
 
 #include "Logger.h"
+
+
+// *==*==*==*==*==*==*==*==*==*==*==*==*
+// ===**    Logger::Scope Class    **===
+// *==*==*==*==*==*==*==*==*==*==*==*==*
+
+// ==================================
+// ===**  Scope Static Members  **===
+// ==================================
+// Static Message Verbosity Levels
+UInt32 Logger::Scope::s_vrb_NONE    = 0;
+UInt32 Logger::Scope::s_vrb_LOW     = 100;
+UInt32 Logger::Scope::s_vrb_MEDIUM  = 200;
+UInt32 Logger::Scope::s_vrb_HIGH    = 300;
+UInt32 Logger::Scope::s_vrb_FULL    = 400;
+UInt32 Logger::Scope::s_vrb_DEBUG   = 500;
+
+// =============================
+// ===**    Constructor    **===
+// =============================
+Logger::Scope::Scope(string iName, UInt32 iLevel)
+{
+  m_name = iName;
+  m_id = s_count++;
+  m_level = iLevel;
+}
+
+// ====================================
+// ===**  Private Static Members  **===
+// ====================================
+
+// =============================
+// ===**      Inits        **===
+// =============================
+
+// =============================
+// ===**  Public Methods   **===
+// =============================
+
+// =============================
+// ===**  Private Methods  **===
+// =============================
+
+// =============================
+// ===**     Operators     **===
+// =============================
+
+// *==*==*==*==*==*==*==*==*==*==*==*==*
+// ===**       Logger Class        **===
+// *==*==*==*==*==*==*==*==*==*==*==*==*
+
+// ====================================
+// ===**  Private Static Members  **===
+// ====================================
+bool Logger::s_initDone = false;
+bool Logger::s_alwaysLogScopes = true;
+
+// =============================
+// ===**      Inits        **===
+// =============================
+void Logger::Init(string iFileName)
+{
+  s_fileName = iFileName;
+  s_oFile.open(s_fileName, ios::in | ios::out | ios::trunc);
+
+  s_initDone = true;
+}
+
+// =============================
+// ===**  Public Methods   **===
+// =============================
+
+// =============================
+// ===**  Private Methods  **===
+// =============================
+
+// =============================
+// ===**     Operators     **===
+// =============================
+void Logger::AddDebugScope(const Scope & iScope)
+{
+  if(&iScope == NULL)
+  {
+    //TBD - log error.
+  }
+  // Check if scope is already in the list.
+  for(list<Scope>::iterator ii=s_scopes.begin(); ii != s_scopes.end(); ++ii)
+  {
+    if((*ii).GetId() == iScope.GetId())
+    {
+      return;
+    }
+  }
+  s_scopes.push_back(iScope);
+  s_scopeNameToIdMap[iScope.GetName()] = iScope.GetId();
+  UInt32 id = iScope.GetId();
+}
+bool Logger::RemoveDebugScope(string iName)
+{
+  bool l_found = false;
+  for(list<Scope>::iterator ii=s_scopes.begin(); ii != s_scopes.end(); ++ii)
+  {
+    if((*ii).GetName() == iName)
+    {
+      l_found = true;
+      s_scopes.erase(ii);
+      break;
+    }
+  }
+  if(l_found)
+  {
+    map<string,UInt32>::iterator kk = s_scopeNameToIdMap.find(iName);
+    if(kk != s_scopeNameToIdMap.end())
+    {
+      s_scopeNameToIdMap.erase(kk);
+    }
+  }
+  return l_found;
+}
