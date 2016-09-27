5fabe1bcd775e53c858c878cec3169ce9d5a6fbf
matthamptonasic <matthamptonasic@gmail.com>
2016-09-27 03:00:01 -0700

    Initial placeholder.

---
+++ verif/Logging/Logger.h
@@ -0,0 +1,23 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Logger.h 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   09/27/16
+#   Description   :   Class/defines for logging to console and file.
+#
+###############################################################################
+*/
+
+#include <iostream>
+#include <iomanip>
+
+class Logger
+{
+
+};
