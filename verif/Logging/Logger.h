01dfbba78d77a81d374db2685fe56fee295259da
matthamptonasic <matthamptonasic@gmail.com>
2016-09-27 06:34:09 -0700

    Scope class and basic members skeleton.

--- verif/Logging/Logger.h
+++ verif/Logging/Logger.h
@@ -14,10 +14,81 @@
 ###############################################################################
 */
 
-#include <iostream>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <map>
+#include <string>
+#include <list>
+
+#include "Common.h"
+
+using namespace std;
 
 class Logger
 {
+  // Enums
+  public:
+
+  // Nested Classes
+  public:
+  class Scope
+  {
+    private:
+      static UInt32 s_vrb_NONE;
+      static UInt32 s_vrb_LOW;
+      static UInt32 s_vrb_MEDIUM;
+      static UInt32 s_vrb_HIGH;
+      static UInt32 s_vrb_FULL;
+      static UInt32 s_vrb_DEBUG;
+      static UInt32 s_count;
+             UInt32 m_id;
+             string m_name;
+             UInt32 m_level;
+
+    public:
+      static UInt32 Vrb_NONE()    { return s_vrb_NONE; }
+      static UInt32 Vrb_LOW()     { return s_vrb_LOW; }
+      static UInt32 Vrb_MEDIUM()  { return s_vrb_MEDIUM; }
+      static UInt32 Vrb_HIGH()    { return s_vrb_HIGH; }
+      static UInt32 Vrb_FULL()    { return s_vrb_FULL; }
+      static UInt32 Vrb_DEBUG()   { return s_vrb_DEBUG; }
+
+      inline UInt32 GetId()       const { return m_id; }
+      inline string GetName()     const { return m_name; }
+      inline UInt32 GetLevel()    const { return m_level; }
+
+    public:
+      Scope(string iName, UInt32 iLevel = s_vrb_DEBUG);
+  };
+
+  // Private Members
+  private:
+    // Logging Control
+    static bool s_initDone;
+    static bool s_alwaysLogScopes;
+    static list<Scope> s_scopes;
+    static map<string, UInt32> s_scopeNameToIdMap;
+
+    // Logger Data
+    static string s_fileName;
+    static fstream s_oFile;
+
+  // Public Properties
+  public:
+    static bool s_AlwaysLogScopes_get() { return s_alwaysLogScopes; }
+    static void s_AlwaysLogScopes_set(bool iValue) { s_alwaysLogScopes = iValue; }
+
+  // Inits
+  public:
+    static void Init(string iFileName);
+
+  // Public Methods
+  public:
+    static void AddDebugScope(const Scope & iScope);
+    static bool RemoveDebugScope(string iName);
+
+  // Private Methods
+  private:
 
 };
