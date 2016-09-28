f9875178c54df24bef535eda7976f57107f6fdc2
matthamptonasic <matthamptonasic@gmail.com>
2016-09-27 22:19:45 -0700

    Added skeleton and getting CL args. Still need to parse CL args to get the individual command line arguments.

--- verif/Test/TestController.cc
+++ verif/Test/TestController.cc
@@ -14,5 +14,71 @@
 ###############################################################################
 */
 
+#include "pli.h"
 #include "TestController.h"
 
+// *==*==*==*==*==*==*==*==*==*==*==*==*
+// ===**   TestController Class    **===
+// *==*==*==*==*==*==*==*==*==*==*==*==*
+
+// ====================================
+// ===**  Private Static Members  **===
+// ====================================
+bool             TestController::s_initDone = false;
+vector<string> * TestController::s_commandLineArgs = nullptr;
+
+// =============================
+// ===** Public Properties **===
+// =============================
+vector<string> & TestController::CommandLineArgs_get()
+{
+  Init();
+  return *s_commandLineArgs;
+}
+
+// =============================
+// ===**    Constructor    **===
+// =============================
+
+// =============================
+// ===**      Inits        **===
+// =============================
+void TestController::Init()
+{
+  if(s_initDone)
+  {
+    return;
+  }
+  commandLineArgs_set();
+
+  s_initDone = true;
+}
+
+// =============================
+// ===**  Public Methods   **===
+// =============================
+void TestController::PrintCommandLineArgs()
+{
+  Init();
+  for(UInt32 ii=0; ii<s_commandLineArgs->size(); ii++)
+  {
+    // TBD - change to logger.
+    Vpi::vpi_printf("Argument[%d] '%s'\n", ii, s_commandLineArgs->at(ii).c_str());
+  }
+}
+
+// =============================
+// ===**  Private Methods  **===
+// =============================
+void TestController::commandLineArgs_set()
+{
+  if(!s_initDone)
+  {
+    s_commandLineArgs = Pli::GetCommandLineArgs();
+  }
+  return;
+}
+
+// =============================
+// ===**     Operators     **===
+// =============================
--- verif/Test/TestController.h
+++ verif/Test/TestController.h
@@ -9,14 +9,50 @@
 #   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
 #   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
 #   Creation Date :   09/15/16
-#   Description   :   
+#   Description   :   Controls the flow of the test.
+#                     This includes which test is run.
 #
 ###############################################################################
 */
 #ifndef TESTCONTROLLER_H
 #define TESTCONTROLLER_H
 
+#include <string>
+#include <vector>
+
 #include "Common.h"
 
-#endif /* TESTCONTROLLER_H */
+using namespace std;
 
+class TestController
+{
+  // Enums
+  public:
+
+  // Nested Classes
+  public:
+
+  // Private Members
+  private:
+    static bool             s_initDone;
+    static vector<string> * s_commandLineArgs;
+
+  // Public Properties
+  public:
+    static vector<string> & CommandLineArgs_get();
+
+  // Inits
+  public:
+    static void Init();
+
+  // Public Methods
+  public:
+    static void PrintCommandLineArgs();
+
+  // Private Methods
+  private:
+    static void commandLineArgs_set();
+
+};
+
+#endif /* TESTCONTROLLER_H */
