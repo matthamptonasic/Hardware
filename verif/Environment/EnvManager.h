5edfc21ce9fa679449895242718e14f913d389b5
matthamptonasic <matthamptonasic@gmail.com>
2016-10-09 23:45:33 -0700

    Added description and some functionality. Added command line arguments. Added logging interface/init.

--- verif/Environment/EnvManager.h
+++ verif/Environment/EnvManager.h
@@ -9,7 +9,9 @@
 #   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
 #   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
 #   Creation Date :   10/06/16
-#   Description   :   
+#   Description   :   This class is the top level coordinator for the entire
+#                     environment. It does all initialization at simulation
+#                     start and runs the test and end of test actions.
 #
 ###############################################################################
 */
@@ -17,6 +19,7 @@
 #define ENVMANAGER_H
 
 #include <string>
+#include <vector>
 
 #include "Common.h"
 
@@ -29,6 +32,11 @@ class EnvManager
 
   // Constants
   private:
+    // ====================================
+    // ===**  Command Line Arguments  **===
+    // ====================================
+    // ex: +c_args="logFile=/home/matt/dump/Hardware/_common/fifo/run.log"
+    const string c_logFile = "logFile"; // path + filename of logfile (all ostream output).
 
   // Nested Classes
   public:
@@ -38,6 +46,7 @@ class EnvManager
 
   // Public Properties
   public:
+    string LogFileName_get() const { return c_logFile; };
 
   // Singleton Pattern
   public:
@@ -55,6 +64,8 @@ class EnvManager
   // Inits
   private:
     void init();
+    bool init_CmdArgs();
+    bool init_Logger();
 
   // Public Methods
   public:
