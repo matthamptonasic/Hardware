65b339a80db0ad2205784e906bcfa7750bffa87d
matthamptonasic <matthamptonasic@gmail.com>
2016-10-09 23:44:28 -0700

    Changed to non-static class. Added constructors and inits. Initialization working, can see both the console output and the file output.

--- verif/Logging/Logger.cc
+++ verif/Logging/Logger.cc
@@ -17,6 +17,11 @@
 #include "Logger.h"
 
 
+// ==================================
+// ===**    Logger Instance     **===
+// ==================================
+Logger * dout;
+
 // *==*==*==*==*==*==*==*==*==*==*==*==*
 // ===**    Logger::Scope Class    **===
 // *==*==*==*==*==*==*==*==*==*==*==*==*
@@ -71,28 +76,113 @@ Logger::Scope::Scope(string iName, UInt32 iLevel)
 // ====================================
 // ===**  Private Static Members  **===
 // ====================================
-bool Logger::s_initDone = false;
-bool Logger::s_alwaysLogScopes = true;
-list<Logger::Scope> Logger::s_scopes;
-map<string, UInt32> Logger::s_scopeNameToIdMap;
 
-string Logger::s_fileName = "";
-fstream Logger::s_oFile;
+
+// =============================
+// ===**   Constructors    **===
+// =============================
+Logger::Logger()
+{
+  init(string(""), nullptr);
+}
+Logger::Logger(string iFileName, ostream * iStream)
+{
+  init(iFileName, iStream);
+}
 
 // =============================
 // ===**      Inits        **===
 // =============================
-void Logger::Init(string iFileName)
+void Logger::init(string iFileName, ostream * iStream)
 {
-  s_fileName = iFileName;
-  s_oFile.open(s_fileName, ios::in | ios::out | ios::trunc);
-
-  s_initDone = true;
+  m_initDone = false;
+  m_fileOut = nullptr;
+  if((iFileName != "") && (iStream != nullptr))
+  {
+    init_streams(iFileName, iStream);
+    m_initDone = true;
+  }
+  m_fileName = "";
+  m_alwaysLogScopes = true;
+}
+void Logger::init_streams(string iFileName, ostream * iStream)
+{
+  SetConsoleOut(iStream);
+  SetFileOut(iFileName);
 }
 
 // =============================
 // ===**  Public Methods   **===
 // =============================
+void Logger::SetConsoleOut(ostream * iStream)
+{
+  m_consoleOut = iStream; // Default is cout.
+  (*m_consoleOut) << __PRETTY_FUNCTION__ << " set." << endl;
+}
+bool Logger::SetFileOut(ofstream * iStream)
+{
+  if(iStream->is_open())
+  {
+    m_fileOut = iStream;
+    return true;
+  }
+  return false;
+}
+void Logger::SetFileOut(string iFileName)
+{
+  m_fileName = iFileName;
+  if(m_fileOut != nullptr)
+  {
+    if(m_fileOut->is_open())
+    {
+      m_fileOut->flush();
+      m_fileOut->close();
+    }
+    delete m_fileOut;
+  }
+  m_fileOut = new ofstream(m_fileName, ios::out | ios::trunc);
+  (*m_fileOut) << __PRETTY_FUNCTION__ << " set." << endl;
+}
+void Logger::AddDebugScope(const Scope & iScope)
+{
+  if(&iScope == NULL)
+  {
+    //TBD - log error.
+  }
+  // Check if scope is already in the list.
+  for(list<Scope>::iterator ii=m_scopes.begin(); ii != m_scopes.end(); ++ii)
+  {
+    if((*ii).GetId() == iScope.GetId())
+    {
+      return;
+    }
+  }
+  m_scopes.push_back(iScope);
+  m_scopeNameToIdMap[iScope.GetName()] = iScope.GetId();
+  UInt32 id = iScope.GetId();
+}
+bool Logger::RemoveDebugScope(string iName)
+{
+  bool l_found = false;
+  for(list<Scope>::iterator ii=m_scopes.begin(); ii != m_scopes.end(); ++ii)
+  {
+    if((*ii).GetName() == iName)
+    {
+      l_found = true;
+      m_scopes.erase(ii);
+      break;
+    }
+  }
+  if(l_found)
+  {
+    map<string,UInt32>::iterator kk = m_scopeNameToIdMap.find(iName);
+    if(kk != m_scopeNameToIdMap.end())
+    {
+      m_scopeNameToIdMap.erase(kk);
+    }
+  }
+  return l_found;
+}
 
 // =============================
 // ===**  Private Methods  **===
@@ -101,43 +191,3 @@ void Logger::Init(string iFileName)
 // =============================
 // ===**     Operators     **===
 // =============================
-void Logger::AddDebugScope(const Scope & iScope)
-{
-  if(&iScope == NULL)
-  {
-    //TBD - log error.
-  }
-  // Check if scope is already in the list.
-  for(list<Scope>::iterator ii=s_scopes.begin(); ii != s_scopes.end(); ++ii)
-  {
-    if((*ii).GetId() == iScope.GetId())
-    {
-      return;
-    }
-  }
-  s_scopes.push_back(iScope);
-  s_scopeNameToIdMap[iScope.GetName()] = iScope.GetId();
-  UInt32 id = iScope.GetId();
-}
-bool Logger::RemoveDebugScope(string iName)
-{
-  bool l_found = false;
-  for(list<Scope>::iterator ii=s_scopes.begin(); ii != s_scopes.end(); ++ii)
-  {
-    if((*ii).GetName() == iName)
-    {
-      l_found = true;
-      s_scopes.erase(ii);
-      break;
-    }
-  }
-  if(l_found)
-  {
-    map<string,UInt32>::iterator kk = s_scopeNameToIdMap.find(iName);
-    if(kk != s_scopeNameToIdMap.end())
-    {
-      s_scopeNameToIdMap.erase(kk);
-    }
-  }
-  return l_found;
-}
