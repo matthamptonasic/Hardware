16db0d83281bb0f58392891c88fb9dac15a2d727
matthamptonasic <matthamptonasic@gmail.com>
2016-10-14 14:51:46 -0700

    Initial stub.

---
+++ verif/Event/Event.cc
@@ -0,0 +1,17 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Event.cc 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   10/14/16
+#   Description   :   
+#
+###############################################################################
+*/
+
+#include "Event.h"
---
+++ verif/Event/Event.h
@@ -0,0 +1,57 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Event.h 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   10/14/16
+#   Description   :   
+#
+###############################################################################
+*/
+
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <list>
+
+#include "Common.h"
+#include "Logger.h"
+
+class Event
+{
+  // Enums
+  public:
+
+  // Nested Classes
+  public:
+
+  // Private Members
+  private:
+
+  // Public Properties
+  public:
+
+  // Constructors
+  public:
+
+  // Inits
+  private:
+
+  // Public Methods
+  public:
+
+  // Private Methods
+  private:
+
+  // Operators
+  public:
+
+};
+
+#endif /* EVENT_H */
+
