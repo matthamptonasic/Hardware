a2d31d4935c13d4d45df732d8e0a227b30bf68e6
matthamptonasic <matthamptonasic@gmail.com>
2016-10-16 00:23:15 -0700

    Added logLvl command line arg check/set.

--- verif/Environment/EnvManager.cc
+++ verif/Environment/EnvManager.cc
@@ -16,12 +16,14 @@
 
 #include <iostream>
 
-#include "TestController.h"
 #include "Logger.h"
+#include "Parsing.h"
+#include "TestController.h"
 
 #include "EnvManager.h"
 
 using namespace std;
+using namespace Text;
 
 // *==*==*==*==*==*==*==*==*==*==*==*==*
 // ===**     EnvManager Class      **===
@@ -69,8 +71,68 @@ bool EnvManager::init_Logger()
     return false;
   }
   Logger * tmp = new Logger(l_logFile, &cout);
-  tmp->SetDout(tmp);
-  
+  tmp->SetAsDout();
+
+  // TBD - check for logging level to set.
+  // i.e. logLvl=250 or logLvl=HIGH (use a *match* on the major levels)
+  // Also support any scope with:
+  // i.e. logScope=TestBase
+  // This is where we need to get tricky and allow a hierarchical scoping.
+  // So, for instance, the user wants to log everything under the TestBase tree.
+  // The inherited classes will all have a different class name.
+  // So how do we enable all of their scopes as well?
+  // Like UVM, we'll have a registry of class objects. This will allow us to 
+  // determine the full hierarchical name of the class.
+  // From there we can determine if the scope we're adding includes this class.
+  // When this is run, we'll go through any objects that are already registered
+  // and enable their scopes.
+  // After this is run, the newly registered objects will need to enable their scopes
+  // while registering.
+
+  // logLvl
+  string l_logLvl = TestController::Access().GetCmdArg_string(c_logLvl);
+  if(l_logLvl == "")
+  {
+    // Try getting a UInt32.
+    UInt32 l_logLvl_UInt32 = TestController::Access().GetCmdArg_UInt32(c_logLvl);
+    if(l_logLvl_UInt32 != 0)
+    {
+      DOUT.VerbosityLevel_set(l_logLvl_UInt32);
+    }
+  }
+  else
+  {
+    if(l_logLvl == "DEBUG")
+    {
+      DOUT.VerbosityLevel_set(Logger::Scope::Vrb_DEBUG());
+    }
+    else if(l_logLvl == "FULL")
+    {
+      DOUT.VerbosityLevel_set(Logger::Scope::Vrb_FULL());
+    }
+    else if(l_logLvl == "HIGH")
+    {
+      DOUT.VerbosityLevel_set(Logger::Scope::Vrb_HIGH());
+    }
+    else if(l_logLvl == "MEDIUM")
+    {
+      DOUT.VerbosityLevel_set(Logger::Scope::Vrb_MEDIUM());
+    }
+    else if(l_logLvl == "LOW")
+    {
+      DOUT.VerbosityLevel_set(Logger::Scope::Vrb_LOW());
+    }
+    else if(l_logLvl == "NONE")
+    {
+      DOUT.VerbosityLevel_set(Logger::Scope::Vrb_NONE());
+    }
+    else
+    {
+      LOG_WRN_ENV << "logLvl passed (" << l_logLvl << ") is not recognized." << endl;
+    }
+  }
+  LOG_MSG << "Verbosity Level is set to " << DOUT.VerbosityLevel_get() << endl;
+
   return true;
 }
 
