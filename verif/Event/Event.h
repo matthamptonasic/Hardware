5cca2ae136a4b1e86ea36d4f168981ca0121744a
matthamptonasic <matthamptonasic@gmail.com>
2016-10-16 21:09:19 -0700

    Adding a delegate and operators working. The () and += operators are functioning. Still can't remove a delegate. Need to figure out how to compare a std::function.target.

--- verif/Event/Event.h
+++ verif/Event/Event.h
@@ -17,11 +17,13 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <functional>
 #include <list>
 
 #include "Common.h"
 #include "Logger.h"
 
+template<typename RetTp, typename ...Args>
 class Event
 {
   // Enums
@@ -32,12 +34,21 @@ class Event
 
   // Private Members
   private:
+    list< function<RetTp(Args...)> > m_list;
 
   // Public Properties
   public:
 
   // Constructors
   public:
+    Event()
+    {
+
+    }
+    Event(function<RetTp(Args...)> iFn)
+    {
+      m_list.push_back(iFn);
+    }
 
   // Inits
   private:
@@ -47,10 +58,54 @@ class Event
 
   // Private Methods
   private:
+    bool add(function<RetTp(Args...)> iFn)
+    {
+      m_list.push_back(iFn);
+    }
+    /*
+    TBD - figure out how to compare targets.
+    bool addIfNot(function<RetTp(Args...)> iFn)
+    {
+      for(typename list<function<RetTp(Args...)>>::iterator ii=m_list.begin(); ii != m_list.end(); ++ii)
+      {
+        if(*ii == iFn)
+        {
+          cout << "found function already in this event." << endl;
+          return false;
+        }
+      }
+      m_list.push_back(iFn);
+      cout << "did not find function, so adding it to this event." << endl;
+      return true;
+    }
+    */
+    bool remove(function<RetTp(Args...)> iFn)
+    {
+      // TBD - After figuring out how to compare targets.
+      LOG_ERR_ENV << "Currently unsupported." << endl;
+    }
+    void fire(Args... iArgs)
+    {
+      for(typename list<function<RetTp(Args...)>>::iterator ii=m_list.begin(); ii != m_list.end(); ++ii)
+      {
+        if(*ii)
+        {
+          (*ii)(iArgs...);
+        }
+      }
+    }
 
   // Operators
   public:
-
+    //PartSelect operator() (UInt32 iUpperIndex, UInt32 iLowerIndex);
+    void operator() (Args... iArgs)
+    {
+      fire(iArgs...);
+    }
+    void operator+= (function<RetTp(Args...)> iFn)
+    {
+      add(iFn);
+    }
 };
 
 #endif /* EVENT_H */
