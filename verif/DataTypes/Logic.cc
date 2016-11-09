fb7f3c1793c01e0dc6672fac13462c05acbf1103
matthamptonasic <matthamptonasic@gmail.com>
2016-11-08 23:48:28 -0800

    Initial.

---
+++ verif/DataTypes/Logic.cc
@@ -0,0 +1,109 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Logic.cc 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   11/08/16
+#   Description   :   
+#
+###############################################################################
+*/
+
+#include <utility>
+
+#include "Logger.h"
+#include "pli.h"
+
+#include "Logic.h"
+
+// ====================================
+// ===**  Private Static Members  **===
+// ====================================
+
+// =============================
+// ===** Public Properties **===
+// =============================
+
+// =============================
+// ===**   Constructors    **===
+// =============================
+Logic::Logic(string iFullName)
+  : TypeBase(iFullName, NB_STATES::TWO_STATE)
+{
+  // Set the bitvector size based on the type and/or size of the verilog object.
+  set_Size();
+  createBV();
+  get_RtlValue();
+}
+
+// =============================
+// ===**      Inits        **===
+// =============================
+
+// =============================
+// ===**  Public Methods   **===
+// =============================
+
+// =============================
+// ===**  Private Methods  **===
+// =============================
+
+// =============================
+// ===** Protected Methods **===
+// =============================
+void Logic::set_Size()
+{
+  // TBD - Get the size based on the verilog object.
+  Vpi::OBJECT l_type = Pli::GetType(get_SigHandle());
+  if(l_type != Vpi::OBJECT::REG)
+  {
+    LOG_WRN_ENV << "Object " << Get_NameFull() << " type (" << (Int32)l_type
+                << ") was not the expected type for a logic object." << endl;
+  }
+  TypeBase::set_Size(Pli::GetSize(get_SigHandle()));
+}
+
+// =============================
+// ===**     Operators     **===
+// =============================
+Logic & Logic::operator=  (UInt32 iRhs)
+{
+  get_BitVector() = iRhs;
+  set_RtlValue();
+  return *this;
+}
+Logic & Logic::operator=  (UInt64 iRhs)
+{
+  get_BitVector() = iRhs;
+  set_RtlValue();
+  return *this;
+}
+Logic & Logic::operator=  (const BitVector & iRhs)
+{
+  get_BitVector() = iRhs;
+  set_RtlValue();
+  return *this;
+}
+Logic & Logic::operator=  (BitVector && iRhs)
+{
+  get_BitVector() = move(iRhs);
+  set_RtlValue();
+  return *this;
+}
+Logic & Logic::operator=  (const Logic & iRhs)
+{
+  get_BitVector() = iRhs.get_BitVector();
+  set_RtlValue();
+  return *this;
+}
+Logic & Logic::operator=  (const TypeBase & iRhs)
+{
+  get_BitVector() = (BitVector)iRhs;
+  set_RtlValue();
+  return *this;
+}
---
+++ verif/DataTypes/Logic.h
@@ -0,0 +1,73 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Logic.h 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   11/08/16
+#   Description   :   
+#
+###############################################################################
+*/
+
+#ifndef LOGIC_H
+#define LOGIC_H
+
+#include <string>
+
+#include "Common.h"
+#include "TypeBase.h"
+
+using namespace std;
+
+class Logic : public TypeBase
+{
+  // Enums
+  public:
+
+  // Private Members
+  private:
+
+  // Public Properties
+  public:
+
+  // Constructors
+  public:
+    Logic(string iFullName);
+
+  // Inits
+  private:
+
+  // Public Methods
+  public:
+
+  // Private Methods
+  private:
+
+  // Protected Methods
+  protected:
+    void set_Size();
+  
+  // Operators
+  public:
+    Logic & operator=  (UInt32 iRhs);
+    Logic & operator=  (UInt64 iRhs);
+    Logic & operator=  (const BitVector & iRhs);
+    Logic & operator=  (BitVector && iRhs);
+    Logic & operator=  (const Logic & iRhs);
+    Logic & operator=  (const TypeBase & iRhs);
+    Logic & operator=  (const TypeBase::PartSelect & iRhs)  { return *this = (BitVector)iRhs; }
+    Logic & operator=  (const BitVector::PartSelect & iRhs) { return *this = (BitVector)iRhs; }
+    Logic & operator=  (long long unsigned int iRhs)        { return *this = (UInt64)iRhs; }
+    Logic & operator=  (long long int iRhs)                 { return *this = (UInt64)iRhs; }
+    Logic & operator=  (Int64 iRhs)                         { return *this = (UInt64)iRhs; }
+    Logic & operator=  (int iRhs)                           { return *this = (UInt32)iRhs; }
+};
+
+typedef Logic logic;
+
+#endif /* LOGIC_H */
