a1e7527b3a56bb30590ef3e836b0a0935ee554aa
matthamptonasic <matthamptonasic@gmail.com>
2016-11-15 23:27:23 -0800

    Initial.

---
+++ verif/DataTypes/Types.h
@@ -0,0 +1,23 @@
+/*
+###############################################################################
+#   Licensing information found at: 
+#     https://github.com/matthamptonasic/Hardware/
+#   In file LICENSING.md
+###############################################################################
+#
+#   File          :   Types.h 
+#   Creator       :   Matt Hampton (matthamptonasic@gmail.com)
+#   Owner         :   Matt Hampton (matthamptonasic@gmail.com)
+#   Creation Date :   11/15/16
+#   Description   :   A common include for our custom types.
+#
+###############################################################################
+*/
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "Bit.h"
+#include "Integer.h"
+#include "Logic.h"
+
+#endif /* TYPES_H */
